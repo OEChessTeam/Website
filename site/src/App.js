b16d3c3fc72eb7ad1d37caf8967d8c2d26a88e13
Rohan C <rcherukuri10@gmail.com>
2022-05-31 23:25:13 -0500

    Scuffed OAuth - only on test + Routing issues

--- site/src/App.js
+++ site/src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dasboard";
 import Preferences from "./components/Preferences/Preferences";
+import Login from "./components/Login/Login";
 
 function App() {
+    const [token, setToken] = useState();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
   return (
-      <></>
+      <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/preferences">
+              <Preferences />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
