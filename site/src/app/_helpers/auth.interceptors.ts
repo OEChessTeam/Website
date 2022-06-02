23645367d2994aa52f7ae8c92a429a867b76e57b
Rohan C <rcherukuri10@gmail.com>
2022-06-02 08:21:51 -0500

    OAuth Verification

--- site/src/app/_helpers/auth.interceptors.ts
+++ site/src/app/_helpers/auth.interceptors.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { TokenStorageService } from '../_services/token-storage.service';
 import { Observable } from 'rxjs';
-const TOKEN_HEADER_KEY = 'Authorization';
+const TOKEN_HEADER_KEY = 'x-access-token';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private token: TokenStorageService) {  }
@@ -11,7 +11,7 @@ export class AuthInterceptor implements HttpInterceptor {
     let authReq = req;
     const token = this.token.getToken();
     if (token != null) {
-      authReq = req.clone({headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+      authReq = req.clone({headers: req.headers.set(TOKEN_HEADER_KEY, token) });
     }
     return next.handle(authReq);
   }
--- site/src/app/register/register.component.ts
+++ site/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  };
 
-  constructor() { }
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage='';
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    const { username, email, password } = this.form;
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
